4ed8158157bd135d13c1b6f8b4d58def54522420
Ian Vos <ian@ianvos.nl>
2020-03-02 20:48:35 +0100

    add electron files

--- monitor/quasar.conf.js
+++ monitor/quasar.conf.js
@@ -47,7 +47,10 @@ module.exports = function (ctx) {
       directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [],
+      config: {
+        dark: 'true' // or Boolean true/false
+      }
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
--- monitor/src/router/routes.js
+++ monitor/src/router/routes.js
@@ -6,6 +6,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
+  },
+  {
+    path: '/settings',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '/settings', component: () => import('pages/Settings.vue') }
+    ]
   }
 ]
 
