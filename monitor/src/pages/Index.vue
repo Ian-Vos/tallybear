62c1dbb735e987897aa5d5e834f47d47dc63c884
Ian Vos <ian@ianvos.nl>
2020-03-20 16:32:08 +0100

    chore: add temporary explanation to cards

--- monitor/src/pages/Index.vue
+++ monitor/src/pages/Index.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="q-pa-lg row items-start q-gutter-lg">
 
+    <!-- Left side of screen -->
     <div class="col-4 row">
       <q-card class="my-card q-pb-lg col-12 q-mb-lg">
         <q-card-section>
-          <div class="text-h6">Controlls</div>
-          These are the most important tally system Controlls
+          <div class="text-h6">Control pannel</div>
+          This section should display the most important information like: <br>
+          speed of the network (if possible maybe try to calculate the differnce between the send and receive time) <br>
+          Status of the controller node <br>
+          Current live camera <br>
         </q-card-section>
       </q-card>
       <q-card class="my-card q-pb-lg col-12">
         <q-card-section>
-          <div class="text-h6">Less used</div>
-          These are the standard not much used tally system Controlls
+          <div class="text-h6">Tally assistent</div>
+          Assistive features to help setup a tally like: <br>
+          Connecting tally to a wifi network <br>
+          Changing LED brightes of all the LED's <br>
+
         </q-card-section>
       </q-card>
     </div>
 
+    <!-- Right of screen -->
     <div class="row col">
       <q-card class="my-card q-pb-lg col-12">
         <q-card-section>
